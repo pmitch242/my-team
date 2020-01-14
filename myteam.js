680a05336113d5e491f06497da4a7066ba3f574b
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-14 17:01:42 -0500

    added comments

--- myteam.js
+++ myteam.js
@@ -7,6 +7,7 @@ const Intern = require("./lib/Intern");
 let id = 0;
 let teamArray = [];
 
+// function to initiate application
 function startApp() {
     console.log("Welcome to My Team");
     console.log("Let's start with the Manager...")
@@ -14,7 +15,10 @@ function startApp() {
     addManager();
 }
 
+// funtion that adds another Employee
 function addAnotherEmployee() {
+
+    // Prompt User
     inquirer.prompt([
         {
             type: "confirm",
@@ -22,9 +26,13 @@ function addAnotherEmployee() {
             name: "anotherEmployee"
         }
     ]).then(confirm => {
+
+        // If user wants to add another employee, call addEmployee function
         if (confirm.anotherEmployee) {
             addEmployee();
         }
+
+        // if user is done adding employees, call createHTML function
         else {
             console.log("Done adding Employees!")
             createHTML();
@@ -32,7 +40,10 @@ function addAnotherEmployee() {
     })
 }
 
+// function to add Engineer
 function addEngineer() {
+
+    // Prompt user
     inquirer.prompt([
         {
             type: "input",
@@ -50,21 +61,29 @@ function addEngineer() {
             name: "engineerGitHub"
         }
     ]).then(answers => {
+
+        // increment id
         id++;
+
+        // store user's answers 
         let engineerName = answers.engineerName;
         let engineerEmail = answers.engineerEmail;
         let engineerGitHub = answers.engineerGitHub;
 
+        // create new engineer using Engineer class
         let engineer = new Engineer(engineerName, id, engineerEmail, engineerGitHub);
 
+        // push engineer into teamArray
         teamArray.push(engineer);
     
-
         addAnotherEmployee();
     })
 }
 
+// function to add Intern
 function addIntern() {
+
+    // prompt user
     inquirer.prompt([
         {
             type: "input",
@@ -82,13 +101,19 @@ function addIntern() {
             name: "internSchool"
         }
     ]).then(answers => {
+
+        // increment id
         id++;
+
+        // store user's answers
         let internName = answers.internName;
         let internEmail = answers.internEmail;
         let internSchool = answers.internSchool;
 
+        // create new intern using Intern class
         let intern = new Intern(internName, id, internEmail, internSchool);
 
+        // push intern into teamArray
         teamArray.push(intern);
     
 
@@ -96,9 +121,11 @@ function addIntern() {
     })
 }
 
+// function that adds general Employee
 function addEmployee() {
-    inquirer.prompt([
 
+    // promt user
+    inquirer.prompt([
         {
             type: "list",
             message: "What is this employee's role?",
@@ -106,17 +133,26 @@ function addEmployee() {
             name: "employeeRole"
         }
     ]).then(answers => {
+        
+        // store user answer
         let employeeRole = answers.employeeRole;
+
+        // call addEngineer function if user wants to add an engineer
         if (employeeRole === "Engineer") {
             addEngineer();
         }
+
+        // call addIntern function if user wants to add an intern
         else {
             addIntern()
         }
     })
 }
 
+// function that adds Manager
 function addManager() {
+
+    // prompt user
     inquirer.prompt([
         {
             type: "input",
@@ -134,16 +170,22 @@ function addManager() {
             name: "managerOfficeNumber"
         }
     ]).then(answers => {
+        // increment id
         id++;
+
+        // store user's answers
         let managerName = answers.managerName;
         let managerEmail = answers.managerEmail;
         let managerOfficeNumber = answers.managerOfficeNumber;
+
+        // create new manager using Manager class
         let manager = new Manager(managerName, id, managerEmail, managerOfficeNumber);
 
+        // push manager into teamArray
         teamArray.push(manager);
 
-    
     }).then(() => {
+        // prompt user
         inquirer.prompt([
             {
                 type: "confirm",
@@ -151,9 +193,13 @@ function addManager() {
                 name: "newManager"
             }
         ]).then(confirm => {
+
+            // call addManager funtion if user wants to add a manager
             if (confirm.newManager) {
                 addManager();
             }
+
+            // call addEmployee funtion if user is done adding managers
             else {
                 console.log("Now to add the rest of your team!");
                 addEmployee();
@@ -162,14 +208,18 @@ function addManager() {
     })
 }
 
+// function to create HTML
 function createHTML(){
     console.log("We are now creating your team's HTML...")
     let cardsHTML = "";
+
+    // get employee's card html
     teamArray.forEach(employee =>{
         let teamCards = employee.generateCard();
         cardsHTML += teamCards;
     });
 
+    // store teamHTML
     let teamHTML = `
     <!DOCTYPE html>
 <html lang="en">
@@ -201,6 +251,7 @@ function createHTML(){
 </html>
     `
 
+    // write html
     fs.writeFile("./output/myteam.html", teamHTML, function(err) {
         if (err) {
            return console.log(err);
@@ -209,4 +260,5 @@ function createHTML(){
      });
 }
 
+// start app
 startApp();
